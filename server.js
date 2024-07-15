09163774250b9de90d60265eccc14e3b478ee143
srudhi6383 <srudhi24@gmail.com>
2024-07-15 14:17:37 +0530

    updated

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const { connectDB } = require('./db');
-const User_Router = require('./routes/userRoutes');
-const { Train_Route } = require('./routes/trainRoutes');
+const cors = require('cors'); 
+const User_Router = require('./Routes/User_routes');
+const { Train_Route } = require('./Routes/Train_routes');
 require('dotenv').config();
 const cookieparser = require('cookie-parser')
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieparser())
-// Use the User Router for the /user route
+
 app.use('/user', User_Router);
-// Use the Train Router for the /train route
-app.use('/train',Train_Route)
-// Basic route to test the server
+app.use('/train',Train_Route);
 app.get('/', (req, res) => {
     res.send('Hi, your Railway server is running');
 });
