38bd5d7eecb74b6b93a20422fa88e2f469016a2d
srudhi6383 <srudhi24@gmail.com>
2024-07-17 16:31:42 +0530

    completed

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGOURL);
+        await mongoose.connect(process.env.MongoURL);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Failed to connect to MongoDB', error);
--- server.js
+++ server.js
@@ -7,18 +7,22 @@ require('dotenv').config();
 const cookieparser = require('cookie-parser')
 const app = express();
 app.use(cors({
-    origin: 'https://starlit-tiramisu-89b68b.netlify.app/', 
+    origin: 'https://master--iridescent-brigadeiros-af24fc.netlify.app/', 
     credentials: true
   }));
 app.use(express.json());
 app.use(cookieparser())
 
+// Use the User Router for the /user route
 app.use('/user', User_Router);
+// Use the Train TRouter for the /train route
 app.use('/train',Train_Route)
+// Basic route to test the server
 app.get('/', (req, res) => {
     res.send('Hi, your Railway server is running');
 });
 
+// Start the server and connect to the database
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
